e92158476e77d7ce650a5fb1e28b84b891212c7b
joaquimgomes8 <joaquimgomes2468@gmail.com>
2026-06-11 23:47:36 -0300

    criação do botao de abrir o vscode

--- system.py
+++ system.py
@@ -1,7 +1,16 @@
 import tkinter as tk
+import subprocess
 root = tk.Tk()
 root.title("Sistema de Máquina")
 root.geometry("400x300")
+
+def abrir_vscode():
+    subprocess.Popen(["C:\\Users\\Joaquim\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"])
+
+button = tk.Button(root, text="Abrir VsCode", command=lambda: abrir_vscode())
+button.pack(pady=20)
+
+
 
 
 
