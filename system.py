e8412fd786119cf597b96e8f2cc53a65feaad7f5
joaquimgomes8 <joaquimgomes2468@gmail.com>
2026-06-13 13:10:47 -0300

    criação da interface do sistema com ikinter.

--- system.py
+++ system.py
@@ -7,12 +7,13 @@ root.geometry("400x300")
 def abrir_vscode():
     subprocess.Popen(["C:\\Users\\Joaquim\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"])
 
-button = tk.Button(root, text="Abrir VsCode", command=lambda: abrir_vscode())
-button.pack(pady=20)
+def abrir_cmd():
+    subprocess.Popen("cmd")
 
+button_vscode = tk.Button(root, text="Abrir VsCode", command=lambda: abrir_vscode())
+button_vscode.pack(pady=20)
 
-
-
-
+button_cmd = tk.Button(root, text="Abrir CMD", command=lambda: abrir_cmd())
+button_cmd.pack(pady=20)
 
 root.mainloop()
